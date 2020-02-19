eba963eb0287c13c522aa4caf038892284647d30
Enrique Rueda <ruedaenr@grinnell.edu>
2020-02-19 15:27:16 -0600

    Completed HW 4

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    @image_array = ['image1.jpg', 'image2.jpg', 'image3.jpg',
+                    'image4.jpg', 'image5.jpg']
+    @rand_result = (0..4).to_a.shuffle.first
   end
 
   def help
